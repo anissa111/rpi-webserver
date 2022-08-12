62828bff7fec7109d69a38f5d74bf6340bdb7542
anissa111 <anissazacharias@gmail.com>
2022-08-12 16:21:47 -0600

    passing info

--- server.py
+++ server.py
@@ -1,9 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
+import datetime
+import os
+
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'Hello World'
+
+def test_server():
+    
+    # get time
+    now = datetime.datetime.now()
+    timestring = now.strftime("%Y-%m-%d %H:%M")
+    
+    # get temperature
+    temp = os.popen("vcgencmd measure_temp").readline().replace("temp=", "")
+    
+    # put together info for templates/index.html
+    template_data = {
+        'title' : 'HELLO!',
+        'time' : timestring,
+        'temp' : temp
+    }
+    
+    return render_template('index.html', **template_data)
 
 if __name__ == '__main__':
     app.run(debug=True, port=80, host='0.0.0.0')
